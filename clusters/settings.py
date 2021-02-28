71101ac18bd93ae4f649a7f804df68a5515073fc
Kami Boers <kamiboers@gmail.com>
2021-02-27 20:06:31 -0700

    add open user endpoint for now

--- clusters/settings.py
+++ clusters/settings.py
@@ -126,5 +126,5 @@ AUTH_USER_MODEL = 'launcher.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 25,
 }
--- launcher/serializers.py
+++ launcher/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 
-from .models import Cluster
+from .models import Cluster, User
 
 class ClusterSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
     model = Cluster
     fields = ['cpus', 'creator', 'memory']
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+  class Meta:
+    model = User
+    fields = ['id', 'username']
