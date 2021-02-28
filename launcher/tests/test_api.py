55f90ce71461241c8fe7ca0ae090963a30ce3039
Kami Boers <kamiboers@gmail.com>
2021-02-28 08:36:33 -0700

    refactor test methods

--- launcher/tests/test_api.py
+++ launcher/tests/test_api.py
@@ -7,38 +7,59 @@ from rest_framework.test import APIRequestFactory
 from launcher.models import Cluster, User
 from launcher.views import cluster_detail, cluster_list
 
+
+
 factory = APIRequestFactory()
 
-# TODO: clean up request/auth functionality
 # TODO: test non authenticated calls
 # TODO: institute token authentication
+
+def _get_all_user_clusters(user):
+    request = factory.get('/clusters/', format='json')
+    force_authenticate(request, user=user)
+    response = cluster_list(request)
+    clusters = response.data
+
+    return response.status_code, clusters
+
+
+def _create_user_cluster(user, cpus=1, memory=1):
+    params = {'creator': user.id, 'cpus': cpus, 'memory': memory}
+    request = factory.post('/clusters/', params, format='json')
+    force_authenticate(request, user=user)
+    response = cluster_list(request)
+    result = response.data
+
+    return response.status_code, result
+
+
+def _get_user_cluster_by_pk(user, cluster_pk):
+    request = factory.get(f'/clusters/{cluster_pk}/', format='json')
+    force_authenticate(request, user=user)
+    return cluster_detail(request, cluster_pk)
+
+
 
 @pytest.mark.django_db
 def test_fetch_all_clusters_via_api(api_client, create_cluster):
     cluster = create_cluster()
-    request = factory.get('/clusters/', format='json')
-    force_authenticate(request, user=cluster.creator)
-    response = cluster_list(request)
-    clusters = response.data
+    status_code, cluster_response = _get_all_user_clusters(cluster.creator)
+    user_cluster = cluster_response[0]
 
-    assert response.status_code == 200
-    assert len(clusters) == 1
-    assert clusters[0]['creator'] == cluster.creator.id
-    assert clusters[0]['cpus'] == cluster.cpus
-    assert clusters[0]['memory'] == cluster.memory
+    assert status_code == 200
+    assert len(cluster_response) == 1
+    assert user_cluster['creator'] == cluster.creator.id
+    assert user_cluster['cpus'] == cluster.cpus
+    assert user_cluster['memory'] == cluster.memory
 
 
 @pytest.mark.django_db
 def test_create_cluster_via_api(api_client, create_user):
     user = create_user
     cpus, memory = 12, 34
-    params = {'creator': user.id, 'cpus': cpus, 'memory': memory}
-    request = factory.post('/clusters/', params, format='json')
-    force_authenticate(request, user=user)
-    response = cluster_list(request)
-    created = response.data
+    status_code, created = _create_user_cluster(user, cpus, memory)
 
-    assert response.status_code == 201
+    assert status_code == 201
     assert created['cpus'] == cpus
     assert created['creator'] == user.id
     assert created['memory'] == memory
@@ -48,24 +69,19 @@ def test_create_cluster_via_api(api_client, create_user):
 def test_invalid_create_cluster_parameters_fail_with_message(api_client, create_user):
     user = create_user
     cpus, memory = 1200, 3400
-    params = {'creator': user.id, 'cpus': cpus, 'memory': memory}
-    request = factory.post('/clusters/', params, format='json')
-    force_authenticate(request, user=user)
-    response = cluster_list(request)
-    error_messages = str(response.data)
+    status_code, result = _create_user_cluster(user, cpus, memory)
+    error_messages = str(result)
 
-    assert response.status_code == 400
-    assert 'CPU value must be between 1-16' in error_messages
-    assert 'Memory value must be between 1-128' in error_messages
+    assert status_code == 400
+    assert f'CPU value must be between 1-16. Invalid value: {cpus}' in error_messages
+    assert f'Memory value must be between 1-128 GiB. Invalid value: {memory}' in error_messages
 
 
 @pytest.mark.django_db
 def test_fetch_cluster_by_id(api_client, create_cluster):
     cluster = create_cluster()
     user = cluster.creator
-    request = factory.get(f'/clusters/{cluster.id}/', format='json')
-    force_authenticate(request, user=cluster.creator)
-    response = cluster_detail(request, cluster.id)
+    response = _get_user_cluster_by_pk(user, cluster.pk)
 
     assert response.status_code == 200
     assert response.data == {
