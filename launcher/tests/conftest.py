9f9c13548e654502910de397e6762c61335903f8
Kami Boers <kamiboers@gmail.com>
2021-02-27 23:01:06 -0700

    pytest config, failing

---
+++ launcher/tests/conftest.py
@@ -0,0 +1,46 @@
+import os
+import django
+import pytest
+import random
+import string
+from rest_framework.test import APIClient
+
+from clusters.settings import BASE_DIR
+
+from launcher.models import Cluster, User
+
+# def pytest_configure():
+#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'launcher.tests.settings')
+#     django.setup()
+
+def random_string(length=15):
+  letters = string.ascii_lowercase
+  return ''.join(random.choice(letters) for i in range(length))
+
+@pytest.fixture
+def create_user(username=random_string()):
+  user = create_user_model(username)
+  yield user
+
+  user.delete()
+
+@pytest.fixture
+def create_cluster(user=None, cpus=1, memory=1):
+  cluster = create_cluster_model(user, cpus, memory)
+  yield cluster
+
+  cluster.delete()
+
+
+def create_client():
+  return APIClient()
+
+def create_user_model(username=random_string()):
+  user = User(username=username)
+  user.save()
+  return user
+
+def create_cluster_model(user=create_user_model(), cpus=1, memory=1):
+  cluster = Cluster(creator=user, cpus=cpus, memory=memory)
+  cluster.save()
+  return cluster
--- launcher/tests/test_api.py
+++ launcher/tests/test_api.py
@@ -1,36 +1,17 @@
-import random
-import string
+import pytest 
 
 from django.test import Client, TestCase
 from django.urls import reverse
-from rest_framework.test import APIClient
 
+from .conftest import create_client
 from launcher.models import Cluster, User
 
-client = APIClient()
+client = create_client()
 
-# TODO: break out tests into separate dir/files, probably create test fixtures
-def random_string(length=15):
-  letters = string.ascii_lowercase
-  return ''.join(random.choice(letters) for i in range(length))
-
-def create_user(username=random_string()):
-  user = User(username=username)
-  user.save()
-  return user
-
-def create_cluster(user, cpus=1, memory=1):
-  cluster = Cluster(creator=user, cpus=cpus, memory=memory)
-  cluster.save()
-  return cluster
-
-def create_client():
-  return Client()
-
-def test_fetch_all_clusters_via_api():
-  client = create_client()
+@pytest.mark.django_db
+def test_fetch_all_clusters_via_api(create_user, create_cluster):
   user = create_user()
-  cluster = create_cluster(user)
+  cluster = create_cluster()
   response = client.get('/clusters/', format='json')
 
   assert response.status_code == 200
