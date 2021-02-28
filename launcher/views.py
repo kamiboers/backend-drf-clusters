011cd7ea6524a78975f57f1481a6de433efd0386
Kami Boers <kamiboers@gmail.com>
2021-02-27 19:49:02 -0700

    cluster model - add viewset and serializers, register in admin

--- launcher/views.py
+++ launcher/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from .models import Cluster
+from .serializers import ClusterSerializer
+
+class ClusterViewSet(viewsets.ModelViewSet):
+  """
+  API endpoint that allows clusters to be created or edited.
+  """
+  queryset = Cluster.objects.all().order_by('-created_at')
+  serializer_class = ClusterSerializer
+  # permission_classes = permissions.isAuthenticated
